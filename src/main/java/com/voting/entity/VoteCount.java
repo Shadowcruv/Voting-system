87eb178f6e968308908d6c9e90b5116b08a48ec5
Miracle Ihediwa <miracleihediwa619@gmail.com>
2024-11-10 13:25:03 +0100

    frontt

--- src/main/java/com/voting/entity/VoteCount.java
+++ src/main/java/com/voting/entity/VoteCount.java
@@ -1,9 +1,6 @@
 package com.voting.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +8,13 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class VoteCount {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private int totalVotes;
     private int trumpVotes;
     private int kamalaVotes;
--- src/main/java/com/voting/service/VotingService.java
+++ src/main/java/com/voting/service/VotingService.java
@@ -7,33 +7,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VotingService {
-    @Autowired
-    private VoteCountRepository voteCountRepository;
 
     public VoteCount getVoteCount() {
-        return voteCountRepository.findById(1L)
-                .orElse(new VoteCount()); // Load existing or create new record
+        return getCurrentVoteCounts();
     }
 
     public void voteFor(String candidate) {
-        VoteCount voteCount = getVoteCount();
-        voteCount.setTotalVotes(voteCount.getTotalVotes() + 1);
 
-        if ("Trump".equalsIgnoreCase(candidate)) {
-            voteCount.setTrumpVotes(voteCount.getTrumpVotes() + 1);
-        } else if ("Kamala".equalsIgnoreCase(candidate)) {
-            voteCount.setKamalaVotes(voteCount.getKamalaVotes() + 1);
-        }
-
-        voteCountRepository.save(voteCount);
     }
 
     public VoteCount getCurrentVoteCounts() {
         // Retrieve votes for each candidate from the database
-        int trumpVotes =  voteCountRepository.findById(1L)
-                .orElse(new VoteCount()).getTrumpVotes();
-        int kamalaVotes =  voteCountRepository.findById(1L)
-                .orElse(new VoteCount()).getKamalaVotes(); // Load existing or create new record
+        int trumpVotes = 9894;
+        int kamalaVotes = 987; // Load existing or create new record
 
         // Calculate the total votes
         int totalVotes = trumpVotes + kamalaVotes;
